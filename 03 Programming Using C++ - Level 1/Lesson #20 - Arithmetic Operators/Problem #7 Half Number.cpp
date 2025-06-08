418e7c1155d54b18df85c6fef08a82ceedac3be1
Ahmed Attay <ahmedattay8@gmail.com>
2025-06-08 14:56:31 +0300

    Add C++ program to calculate half of a user-input number

---
+++ 03 Programming Using C++ - Level 1/Lesson #20 - Arithmetic Operators/Problem #7 Half Number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int FirstNumber = 0;
+    cout << "Please enter a number: ";
+    cin >> FirstNumber;
+    int HalfNumber = FirstNumber / 2;
+    cout << "Half of the number is: " << HalfNumber << endl;
+    return 0;
+}
