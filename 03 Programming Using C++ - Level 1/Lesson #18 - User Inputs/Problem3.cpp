ec8bb9a71b07485cf7052146870b116e711af2cb
Ahmed Attay <ahmedattay8@gmail.com>
2025-06-08 13:42:03 +0300

    Solve problem 3

---
+++ 03 Programming Using C++ - Level 1/Lesson #18 - User Inputs/Problem3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout << "Please enter your age: ";
+    cin >> age;
+    cout << "\nCalculation Result:\n";
+    cout << "---------------------\n";
+    cout << "Your age after 10 years will be: " << age + 10 << endl;
+    cout << "---------------------\n";
+    cout << "Ahmed Attay Kamal" << endl;
+    return 0;
+}
